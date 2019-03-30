bcf5f81fe1f885809a2ee51851969c121681547f
Peter <46075072+macropeter@users.noreply.github.com>
2019-03-30 20:51:37 +0100

    korrigiert nach Fisher-Yates-Knuth

--- shuffle.php
+++ shuffle.php
@@ -5,30 +5,32 @@
 
 <body>
 <?php
+
+/*Knuth-Shuffle: man kann beweisen, dass jede
+  Möglichkeit gleich wahrscheinlich ist*/
+
 $n=10;//Anzahl der Elemente
 $x=[]; //leeres Array erstellen
 for ($i=0;$i<$n;$i++)
 {
-    $x[]=$i;
+    $x[]=$i; //push: ein Element an das Array anhängen
 }
-var_dump($x);
+//var_dump($x);
 
-for ($i=0;$i<1000;$i++)
+for ($i=0;$i<$n-1;$i++)
 {
-    $p1=rand(0,$n-1);
-    $p2=rand(0,$n-1);
-    //Dreieckstausch:
-    $h=$x[$p1];
-    $x[$p1]=$x[$p2];
-    $x[$p2]=$h;
-}
 
+    $r=rand($i,$n-1);
+    //Dreieckstausch:
+    $h=$x[$i];
+    $x[$i]=$x[$r];
+    $x[$r]=$h;
+}
+//Ausgabe:
 foreach ($x as $item)
 {
     echo("<p>$item</p>");
 }
-
-
 
 ?>
 </body>
